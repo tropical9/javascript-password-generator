a24adff90da12be8ac0e53921a424ea5f5eaead7
tropical9 <113566952+tropical9@users.noreply.github.com>
2022-11-06 22:51:06 -0800

    javascript update

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// JAVASCRIPT KEYWORDS
+// var	Declares a variable
+// let	Declares a block variable
+// const	Declares a block constant
+// if	Marks a block of statements to be executed on a condition
+// switch	Marks a block of statements to be executed in different cases
+// for	Marks a block of statements to be executed in a loop
+// function	Declares a function
+// return	Exits a function
+// try	Implements error handling to a block of statements
+
+var generateBtn = document.querySelector ("#generate");
+
+var length;
+var upperCase;
+var lowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numeric = [0,1,2,3,4,5,6,7,8,9];
+var specialCharacter = [" ", "!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<",">","=","?","@","[","]","\:","^","_","`","{","|","}","~"]; 
